af72c189795d4939172674e93cf8f3e9d10b9099
Tejasnavadkar <tejas.navadkar@gmail.com>
2025-08-22 15:06:47 +0530

    swiper-js for sliders

---
+++ SwiperJs_demo/script.js
@@ -0,0 +1,7 @@
+const swiper = new Swiper(".mySwiper",{
+     navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+})
+// for more sliders doc -  https://swiperjs.com/get-started
